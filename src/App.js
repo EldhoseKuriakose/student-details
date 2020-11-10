97ff705a02e35ebb625d1a0bc49f58346777d598
Eldhose Kuriakose <eldhosekuriakose96@gmail.com>
2020-11-10 15:43:31 +0530

    implemented login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage.component';
+import LoginPage from './pages/LoginPage/LoginPage.component';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
       </Switch>
     </div>
   );
